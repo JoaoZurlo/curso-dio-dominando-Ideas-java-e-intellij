ade7fb6580ff13c98a91d59ad6f8c37baec5dbaf
JoaoZurlo <jpzurlo.jz@gmail.com>
2023-02-23 13:17:31 -0300

    First commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+       /* int a = 3;
+        int b = 5;
+        System.out.println("Hello Word " + (a+b));*/
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+    }
+}
